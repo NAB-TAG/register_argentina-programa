7b90339a2f734b91166cbc85c74c2a37cede478b
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2023-11-09 12:04:21 -0300

    Subiendo el diseño

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductDetail from './pages/ProductDetail'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <ProductDetail />
+    </>
+  )
+}
+
+export default App
